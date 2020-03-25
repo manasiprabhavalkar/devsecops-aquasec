a09c4f7f696c4cac7eb0f6cc61909acdca8802e9
Irshad A Buchh <ibuchh@gmail.com>
2020-03-25 09:53:55 -0500

    Update deploy-devsecops.sh

--- cloudformation/deploy-devsecops.sh
+++ cloudformation/deploy-devsecops.sh
@@ -4,7 +4,7 @@ REGION="us-west-2"
 GITHUB_USER="ibuchh"
 GITHUB_REPO="devsecops-twistlock"
 GITHUB_TOKEN=""
-# create the base VPC and Subnets (public and private)
+# create the base VPC, Subnets, ECS (Fragate) cluster, CodePipeline and ECR
 aws --region $REGION cloudformation create-stack \
 --stack-name ${DEVSECOPS_STACK} \
 --template-body file://./devsecops.yml \
